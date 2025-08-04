17f1b0ff6bb766a69afca3045a7e835a56da3578
Pedro Pinheiro <pedropinheiro.dev@gmail.com>
2025-08-04 13:23:45 -0300

    Refactor pagination loading state checks to use imported LoadState

--- app/src/main/java/com/org/rickandmorty/utils/PaginationUtils.kt
+++ app/src/main/java/com/org/rickandmorty/utils/PaginationUtils.kt
@@ -1,9 +1,10 @@
 package com.org.rickandmorty.utils
 
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 
 val LazyPagingItems<*>.isLoading: Boolean
-    get() = loadState.refresh is androidx.paging.LoadState.Loading
+    get() = loadState.refresh is LoadState.Loading
 
 val LazyPagingItems<*>.isLoadingMore: Boolean
-    get() = loadState.append is androidx.paging.LoadState.Loading
+    get() = loadState.append is LoadState.Loading
